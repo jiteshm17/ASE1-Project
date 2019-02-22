64c19903c920bd1a6fba4823855e0a3f3b4e0331
= <jiteshmalipeddi92@gmail.com>
2019-02-22 13:10:17 +0530

    Changed db

--- ASE1/settings.py
+++ ASE1/settings.py
@@ -96,7 +96,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'db.sqlite3',
     }
 }
 
